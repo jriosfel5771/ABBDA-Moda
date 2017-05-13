57e86a66a58179c7eb8ceb6006d99699b0b00d85
jriosfel5771 <jriosfel5771@universidadean.edu.co>
2017-05-13 16:30:34 -0500

    Proyecto ABBDA Moda

---
+++ ABBDA-Moda/functions.js
@@ -0,0 +1,30 @@
+function myMap()
+{
+  myCenter=new google.maps.LatLng(4.643885, -74.099783);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
+// Toggle between hiding and showing blog replies/comments
+document.getElementById("myBtn").click();
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function likeFunction(x) {
+    x.style.fontWeight = "bold";
+    x.innerHTML = "✓ Te gusta";
+}
